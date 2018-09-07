6fd908447611ec02d27d633a6516e3fe66f8ffa8
fardog <nate@milkandtang.com>
2018-09-06 17:28:17 -0700

    fix flipped keys

--- keyboards/ts65avr/keymaps/fardog/keymap.c
+++ keyboards/ts65avr/keymaps/fardog/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_END, \
         KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT, KC_PGUP, \
         KC_LSFT,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP, KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT, MO_FUNC,  KC_SPC,                    KC_SPC,          MO_FUNC, KC_RALT, KC_NO,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT, MO_FUNC,  KC_SPC,                    KC_SPC,          KC_RALT, MO_FUNC, KC_NO,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FUNC] = KEYMAP( \
          KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL, _______, \
