27afd704bec10c6856747ae5a2b1fc6e41a33b9a
Nathan wittstock <nate@fardog.io>
2020-09-20 12:23:46 -0700

    add windows mode

--- keyboards/ts65avr/keymaps/fardog/keymap.c
+++ keyboards/ts65avr/keymaps/fardog/keymap.c
@@ -2,10 +2,13 @@
 
 #define _______ KC_TRNS
 
-#define _QWERTY 0
-#define _FUNC 1
+#define _LINUX 0
+#define _WINDOWS 1
+#define _FUNC 2
 
 #define MO_FUNC MO(_FUNC)
+#define DF_LIN DF(_LINUX)
+#define DF_WIN DF(_WINDOWS) 
 
 //enum custom_keycodes {
 //    QWERTY = SAFE_RANGE,
@@ -13,12 +16,19 @@
 //};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: qwerty */
-    [_QWERTY] = KEYMAP( \
+    [_LINUX] = KEYMAP( \
      /* <------------- LEFT SIDE ------------------------------------> <------------------------- RIGHT SIDE ------------------------------------------> */
          KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, KC_HOME, \
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_END, \
         KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT, KC_PGUP, \
+        KC_LSFT,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP, KC_PGDN, \
+        KC_LCTL, KC_LGUI, KC_LALT, MO_FUNC,  KC_SPC,                    KC_SPC,          KC_RALT, MO_FUNC, KC_NO,            KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [_WINDOWS] = KEYMAP( \
+     /* <------------- LEFT SIDE ------------------------------------> <------------------------- RIGHT SIDE ------------------------------------------> */
+         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, KC_HOME, \
+         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_END, \
+        KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT, KC_PGUP, \
         KC_LSFT,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP, KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT, MO_FUNC,  KC_SPC,                    KC_SPC,          KC_RALT, MO_FUNC, KC_NO,            KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -27,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,          KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-          RESET,   DEBUG, _______, _______, _______,                   _______,          KC_RCTL, _______, _______, _______,          _______, _______)
+          RESET,   DEBUG, _______, _______, _______,                   _______,          KC_RCTL, _______, _______,          DF_LIN,  _______, DF_WIN)
 };
 
 const uint16_t PROGMEM fn_actions[] = {};
